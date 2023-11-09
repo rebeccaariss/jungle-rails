970faba041bd51997a325c0798db4fb3a710a93e
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-11-09 17:32:12 -0500

    Add view file for signup form; add logic for 'create' on users controller and private 'user_params' method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,19 @@ class UsersController < ApplicationController
 
   # Receives form and creates user with form's parameters:
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
