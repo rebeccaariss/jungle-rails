6c6275cb9c12b48d0b60fdc6500361850048efb9
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-11-09 18:37:07 -0500

    Add bcrypt authentication methods for User; add sessions controller; add sessions view (login form)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create # (aka login)
+  end
+
+  def destroy # (aka logout)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  # Gives the User model authentication methods via bcrypt:
+  has_secure_password
+
 end
